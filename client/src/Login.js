8cc83f111e9d2cd3b3768ada1a6fcd5ac29c45ef
pravash <pravashbehera2@gmail.com>
2019-03-24 08:26:50 +0530

    make it better

--- client/src/Login.js
+++ client/src/Login.js
@@ -18,139 +18,139 @@ import setAuthToken from "./utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 
 const styles = theme => ({
-  main: {
-    width: "auto",
-    display: "block", // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto"
-    }
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
-      .spacing.unit * 3}px`
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing.unit
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3
-  }
+	main: {
+		width: "auto",
+		display: "block", // Fix IE 11 issue.
+		marginLeft: theme.spacing.unit * 3,
+		marginRight: theme.spacing.unit * 3,
+		[theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+			width: 400,
+			marginLeft: "auto",
+			marginRight: "auto"
+		}
+	},
+	paper: {
+		marginTop: theme.spacing.unit * 8,
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+			.spacing.unit * 3}px`
+	},
+	avatar: {
+		margin: theme.spacing.unit,
+		backgroundColor: theme.palette.secondary.main
+	},
+	form: {
+		width: "100%", // Fix IE 11 issue.
+		marginTop: theme.spacing.unit
+	},
+	submit: {
+		marginTop: theme.spacing.unit * 3
+	}
 });
 
 class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
+		this.state = {
+			email: "",
+			password: ""
+		};
+	}
 
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
+	validateForm() {
+		return this.state.email.length > 0 && this.state.password.length > 0;
+	}
 
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  };
+	handleChange = event => {
+		this.setState({
+			[event.target.id]: event.target.value
+		});
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
-    const data = {
-      registrationNumber: this.state.email,
-      employeeId: this.state.email,
-      parentId: this.state.email,
-      password: this.state.password
-    };
-    axios
-      .post(`http://localhost:3000/${this.props.match.params.id}/login`, data)
-      .then(res => {
-        // Save to localStorage
-        const { token } = res.data;
-        // Set token to ls
-        localStorage.setItem("jwtToken", token);
-        // Set token to Auth header
-        setAuthToken();
-        // Decode token to get user data
-        const decoded = jwt_decode(token);
-        this.props.history.push(`/${this.props.match.params.id}`);
-      })
-      .catch(function(err) {
-        alert(err);
-      });
-  };
+	handleSubmit = event => {
+		event.preventDefault();
+		const data = {
+			registrationNumber: this.state.email,
+			employeeId: this.state.email,
+			parentId: this.state.email,
+			password: this.state.password
+		};
+		axios
+			.post(`/${this.props.match.params.id}/login`, data)
+			.then(res => {
+				// Save to localStorage
+				const { token } = res.data;
+				// Set token to ls
+				localStorage.setItem("jwtToken", token);
+				// Set token to Auth header
+				setAuthToken();
+				// Decode token to get user data
+				const decoded = jwt_decode(token);
+				this.props.history.push(`/${this.props.match.params.id}`);
+			})
+			.catch(function(err) {
+				alert(err);
+			});
+	};
 
-  render() {
-    const { classes } = this.props;
-    console.log(this.state);
-    return (
-      <main className={classes.main}>
-        <CssBaseline />
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form onSubmit={this.handleSubmit} className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">User Id</InputLabel>
-              <Input
-                value={this.state.email}
-                onChange={this.handleChange}
-                id="email"
-                name="email"
-                autoComplete="email"
-              />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input
-                value={this.state.password}
-                onChange={this.handleChange}
-                name="password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}>
-              Sign in
-            </Button>
-          </form>
-        </Paper>
-      </main>
-    );
-  }
+	render() {
+		const { classes } = this.props;
+		console.log(this.state);
+		return (
+			<main className={classes.main}>
+				<CssBaseline />
+				<Paper className={classes.paper}>
+					<Avatar className={classes.avatar}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<form onSubmit={this.handleSubmit} className={classes.form}>
+						<FormControl margin="normal" required fullWidth>
+							<InputLabel htmlFor="email">User Id</InputLabel>
+							<Input
+								value={this.state.email}
+								onChange={this.handleChange}
+								id="email"
+								name="email"
+								autoComplete="email"
+							/>
+						</FormControl>
+						<FormControl margin="normal" required fullWidth>
+							<InputLabel htmlFor="password">Password</InputLabel>
+							<Input
+								value={this.state.password}
+								onChange={this.handleChange}
+								name="password"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+							/>
+						</FormControl>
+						<FormControlLabel
+							control={<Checkbox value="remember" color="primary" />}
+							label="Remember me"
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							className={classes.submit}>
+							Sign in
+						</Button>
+					</form>
+				</Paper>
+			</main>
+		);
+	}
 }
 SignIn.propTypes = {
-  classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(SignIn);
